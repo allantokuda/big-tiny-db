7c5face06759ec078bc1065a336ab47d51660a03
Allan Tokuda <allan.tokuda@gmail.com>
2024-11-30 15:13:01 -0600

    Achieving creatures of each skill

--- activerecord/models/skill.rb
+++ activerecord/models/skill.rb
@@ -2,6 +2,7 @@ require 'active_record'
 
 class Skill < ActiveRecord::Base
   has_many :achievements, foreign_key: :s_code
+  has_many :achieving_creatures, through: :achievements, source: :creature
 
   def description
     s_desc
--- activerecord/spec/skill_spec.rb
+++ activerecord/spec/skill_spec.rb
@@ -12,4 +12,9 @@ describe Skill do
     expect(skill.description).to eq('Walk on Water')
     expect(skill.achievements.count).to eq(0)
   end
+
+  it 'can have many achieving creatures' do
+    skill = Skill.find('A')
+    expect(skill.achieving_creatures.map(&:c_id).sort).to eq([1, 2, 3, 4])
+  end
 end
