ce63ac87f276bd623cf4ff526e5da83290e70e52
Allan Tokuda <allan.tokuda@gmail.com>
2024-11-30 14:49:20 -0600

    Inverse bossed creatures

--- activerecord/models/creature.rb
+++ activerecord/models/creature.rb
@@ -4,6 +4,7 @@ class Creature < ActiveRecord::Base
   has_many :achievements, foreign_key: :c_id
   has_one :mayor_town, class_name: 'Town', foreign_key: :mayor_c_id
   belongs_to :boss, class_name: 'Creature', foreign_key: :boss_c_id
+  has_many :bossed_creatures, class_name: 'Creature', foreign_key: :boss_c_id
 
   def name
     c_name
--- activerecord/spec/creature_spec.rb
+++ activerecord/spec/creature_spec.rb
@@ -19,12 +19,19 @@ describe Creature do
     expect(creature.mayor_town.name).to eq('Anoka')
   end
 
-  it 'can have a boss' do
+  it 'can have a boss creature' do
     creature = Creature.find(1)
     expect(creature.name).to eq('Bannon')
     expect(creature.boss.name).to eq('Carlis')
     expect(creature.boss.boss.name).to eq('Neff')
     expect(creature.boss.boss.boss.name).to eq('Kermit')
     expect(creature.boss.boss.boss.boss).to be_nil
+  end
+
+  it 'can have bossed creatures' do
+    creature = Creature.find(7)
+    expect(creature.name).to eq('Kermit')
+    expect(creature.bossed_creatures.map(&:name)).to eq(%w[Neff])
+    expect(creature.bossed_creatures.flat_map(&:bossed_creatures).map(&:name).sort).to eq(%w[Carlis Mieska])
   end
 end
